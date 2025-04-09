6032135a23aa7c7289338dc1b2f842b2fdf4b712
youxx329 <younggu329@naver.com>
2025-04-09 13:46:41 +0900

    json-server 배포

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -5,7 +5,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const getProduct = async () => {
-    let url = `http://localhost:5000/products`;
+    let url = `https://my-json-server.typicode.com/youxx329/youxx-hnm/products`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
